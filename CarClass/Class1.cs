56b8f406b37c470ca8b85c2f351c1021d54c2184
Артём_ПК <_@WIN-1CE93UBQJN5>
2020-06-05 21:30:27 +0400

    Добавьте файлы проекта.

---
+++ CarClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Car
+    {
+        public double price { get; set; }
+        public int age { get; set; }
+        public string place { get; set; }
+        public string name { get; set; }
+
+        public void GetCarInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Лет с момента покупки: {age}| |Страна производства: {place}|");
+        }
+    }
+
+
+    class Car1 : Car
+    {
+        public string type { get; set; }
+
+        public virtual void GetCar1Info()
+        {
+            Console.WriteLine($"Имя: {name}| |Лет с момента покупки: {age}| |Место: {place}| |Цена автомобиля: {price}| |Тип: {type}");
+        }
+    }
+}
---
+++ CarClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+            car.name = "Q7";
+            car.price = 3000000;
+            car.age = 1;
+            car.place = "Германия";
+
+            car.GetCarInfo();
+
+            Car1 car1 = new Car1();
+            car1.type = "Купэ";
+            car1.name = "Porshe cayman";
+            car1.place = "Германия";
+            car1.price = 5200000;
+            car1.age =  3;
+
+
+            car1.GetCar1Info();
+
+            Console.ReadLine();
+        }
+    }
+}
